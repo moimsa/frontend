c295e682908fb6f5372720bfc51f8caef63b4c26
D.seok <ehskstkrjsa@naver.com>
2024-12-14 15:16:39 +0900

    React Basic Setting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,7 @@
 import './App.css';
-import './css/common/_Reset.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hellow!</h1>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
